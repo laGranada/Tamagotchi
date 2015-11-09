62985e853ec0460db4677073101af77dd2fab406
amelie-beh <amelie-behrendt@web.de>
2015-11-09 20:27:43 +0100

    Javascript eingebunden

---
+++ Quellcode/javascript.js
@@ -0,0 +1,10 @@
+	var sleep = 100;
+	var food = 100;
+	var health = 100;
+	var fun = 100;
+
+	*/ Klick auf Sleep Button, füllt die Energie wieder auf /*
+	function sleep() {
+		var sleep = 100;
+		alert("Energy is full!");
+		}
